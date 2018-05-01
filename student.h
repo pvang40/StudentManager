acf55d87e950aa9355b075060c37e24ab821f442
pvang40 <pvang40@mail.csuchico.edu>
2018-04-30 17:10:06 -0700

    .h file

---
+++ student.h
@@ -0,0 +1,17 @@
+#ifnedf _STUDENT_H_ 
+#define _STUDENT_H_ 
+
+class student 
+{ 
+private:
+    string last; 
+    string first; 
+
+public:
+    void fullName(); 
+    void setName(); 
+        
+}; 
+
+
+#endif
